1dd9e7a026ca15e1740e9cf5b567a2dada0bd55b
Bianca Castanheira <castanheira.bi@gmail.com>
2021-06-04 16:52:24 -0300

    ✨ Criei a lógica do Card de  consulta da Agenda.

--- src/pages/schedule/index.js
+++ src/pages/schedule/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import AppointmentCard from "../../components/AppointmentCard";
+import { getAppointments } from "../../api/AppointmentApi";
 
 const Schedule = () => {
-  return <div>Schedule</div>;
+  const [loading, setLoading] = useState(false);
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    getAppointments()
+      .then((response) => {
+        setAppointments(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Erro ao pegar os agendamentos.");
+      });
+  }, []);
+
+  return (
+    <div>
+      {loading ? <div>Carregando...</div> : ""}
+      {appointments.map((appointment) => (
+        <AppointmentCard key={appointment.id} appointment={appointment} />
+      ))}
+      {/* {dummyAppointments.map((appointment) => (
+        <AppointmentCard appointment={appointment} />
+      ))} */}
+    </div>
+  );
 };
 
 export default Schedule;
