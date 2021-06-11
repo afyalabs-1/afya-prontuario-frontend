d1c938dfaef97a33d0d4fdb74c709c75bd480fc0
Bianca Castanheira <castanheira.bi@gmail.com>
2021-06-11 19:42:38 -0300

    🚧 Configuração botão prontuário.

--- src/api/Api.js
+++ src/api/Api.js
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: "https://afya-challenge.herokuapp.com",
   headers: {
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Im92X1RvMEdDbzNWcFJQM0YiLCJpYXQiOjE2MjMzNjQwMDIsImV4cCI6MTYyMzQ1MDQwMn0.ia5Y3Aowrt_De2xDA32p4ebNbxBdVpG_b1P33-lcVMY",
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Im92X1RvMEdDbzNWcFJQM0YiLCJpYXQiOjE2MjM0NTExNjAsImV4cCI6MTYyNDA1NTk2MH0.IIx5Av99y2Zk9jRimkuBPw-BhO5vyJjyymBlN4jq4V0",
   },
 });
