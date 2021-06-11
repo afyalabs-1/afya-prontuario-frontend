d577fff3d8a4832524a09b6a9938305406e46a4c
Bianca Castanheira <castanheira.bi@gmail.com>
2021-06-10 22:00:09 -0300

    ✨ Início da construção página clientes.

--- src/pages/customer/index.js
+++ src/pages/customer/index.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import { getClients } from "../../api/ClientApi";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: "white",
+  },
+}));
 
 const Customer = () => {
+  const classes = useStyles();
+
+  // useEffect(() => {
+  //   getClients()
+  //     .then((response) => {
+  //       console.log(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Erro na requisição dos clientes");
+  //     });
+  // }, []);
+
   return (
-    <div className='css-provisorio'>
-      <h1>Cliente</h1>
+    <div className={classes.root}>
+      <List>
+        <ListItem variant="outlined">
+          <Typography>Teste</Typography>
+        </ListItem>
+      </List>
     </div>
   );
 };
