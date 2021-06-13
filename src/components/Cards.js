5748010252d6ce1cb18f8cce8bd59d516328f2a6
Melissa Ribeiro <melissaribeiro@usp.br>
2021-06-13 20:38:44 -0300

    🎨 edição de layout da dashboard

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import Container from "@material-ui/core/Container";
 
 import Grid from "@material-ui/core/Grid";
 
@@ -14,10 +15,11 @@ import Typography from "@material-ui/core/Typography";
 
 import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
+import Box from "@material-ui/core/Box";
 
 const useStyles = makeStyles({
   card: {
-    backgroundColor: "#EEE8EA",
+    backgroundColor: "#fff",
   },
   media: {
     height: 300,
@@ -28,114 +30,112 @@ export const Cards = () => {
   const classes = useStyles();
 
   return (
-    <div>
-      <Grid container spacing={3}>
-        <Grid item xs={6}>
-          <Link to="/customer">
-            <Card className={classes.card}>
-              <CardContent>
-                <Grid container spacing={2}>
-                  <Grid item>
-                    <PersonIcon style={{ fontSize: 80 }}></PersonIcon>
-                  </Grid>
-                  <Grid item xs={12} sm container>
-                    <Grid item xs container direction="column" spacing={2}>
-                      <Grid item xs>
-                        <Typography variant="h6">Clientes</Typography>
-                        <Typography variant="body2" color="textSecondary">
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et
-                          dolore magna aliqua. Ut enim ad minim veniam
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </Grid>
-              </CardContent>
-            </Card>
-          </Link>
-        </Grid>
+    <Container>
+      <Box display="flex" justifyContent="flex-end" m={1} p={1}>
+        <Grid container spacing={3}>
 
-        <Grid item xs={6}>
-          <Link to="/newAppointment">
-            <Card className={classes.card}>
-              <CardContent>
-                <Grid container spacing={2}>
-                  <Grid item>
-                    <BusinessCenterIcon
-                      style={{ fontSize: 80 }}
-                    ></BusinessCenterIcon>
-                  </Grid>
-                  <Grid item xs={12} sm container>
-                    <Grid item xs container direction="column" spacing={2}>
-                      <Grid item xs>
-                        <Typography variant="h6">Novo atendimento</Typography>
-                        <Typography variant="body2" color="textSecondary">
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et
-                          dolore magna aliqua. Ut enim ad minim veniam
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </Grid>
-              </CardContent>
-            </Card>
-          </Link>
-        </Grid>
+          {/* Caso não vá utilizar as estatisticas de pieChart,
+              utilizar o código abaixo */}
 
-        <Grid item xs={6}>
-          <Link to="/medicalRecord">
-            <Card className={classes.card}>
+          {/* <Grid item xs={12}>
+            <Card variant="outlined">
               <CardContent>
-                <Grid container spacing={2}>
-                  <Grid item>
-                    <DescriptionIcon style={{ fontSize: 80 }}></DescriptionIcon>
-                  </Grid>
-                  <Grid item xs={12} sm container>
-                    <Grid item xs container direction="column" spacing={2}>
-                      <Grid item xs>
-                        <Typography variant="h6">Prontuários</Typography>
-                        <Typography variant="body2" color="textSecondary">
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et
-                          dolore magna aliqua. Ut enim ad minim veniam
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </Grid>
+                <Typography variant="h4">Bem vindo, AfyaGama! </Typography>{" "}
+                <br />
+                <Typography align="justify" variant="body1">
+                  Realize o gerenciamento de atendimentos da sua clinica médica.
+                  Crie perfis de novos clientes, agende novas consultas,
+                  cadastre novos profissionais de saúde, e mantenha os dados de
+                  clientes quanto de especialistas atualizado. Todos esses dados
+                  serão salvos em uma banco de dados que pode ser acessado
+                  apenas por rotas autorizadas para fazer suas requisições.
+                </Typography>
               </CardContent>
             </Card>
-          </Link>
-        </Grid>
+          </Grid> */}
 
-        <Grid item xs={6}>
-          <Link to="/schedule">
-          <Card className={classes.card}>
-              <CardContent>
-                <Grid container spacing={2}>
-                  <Grid item>
-                    <EventIcon style={{ fontSize: 80 }}></EventIcon>
-                  </Grid>
-                  <Grid item xs={12} sm container>
-                    <Grid item xs container direction="column" spacing={2}>
-                      <Grid item xs>
-                        <Typography variant="h6">Agenda</Typography>
-                        <Typography variant="body2" color="textSecondary">
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et
-                          dolore magna aliqua. Ut enim ad minim veniam
-                        </Typography>
+          <Grid item xs={4}>
+            <Link to="/customer">
+              <Card className={classes.card} variant="outlined">
+                <CardContent>
+                  <Grid container spacing={2}>
+                    <Grid item>
+                      <PersonIcon style={{ fontSize: 80 }}></PersonIcon>
+                    </Grid>
+                    <Grid item xs={12} sm container>
+                      <Grid item xs container direction="column" spacing={2}>
+                        <Grid item xs>
+                          <Typography variant="h6">
+                            Cadastrar clientes
+                          </Typography>
+                          <Typography variant="body2">
+                            A ficha cadastral deve ser preenchida com nome
+                            completo, documentos de identificação, dados de
+                            contato e endereço.
+                          </Typography>
+                        </Grid>
                       </Grid>
                     </Grid>
                   </Grid>
-                </Grid>
-              </CardContent>
-            </Card>
-          </Link>
+                </CardContent>
+              </Card>
+            </Link>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Link to="/newAppointment">
+              <Card className={classes.card} variant="outlined">
+                <CardContent>
+                  <Grid container spacing={2}>
+                    <Grid item>
+                      <BusinessCenterIcon
+                        style={{ fontSize: 80 }}
+                      ></BusinessCenterIcon>
+                    </Grid>
+                    <Grid item xs={12} sm container>
+                      <Grid item xs container direction="column" spacing={2}>
+                        <Grid item xs>
+                          <Typography variant="h6">Novo atendimento</Typography>
+                          <Typography variant="body2">
+                            Anote as principais queixas, solicitação de exames
+                            e/ou atividades de enfermagem necessárias para
+                            admissão.
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </CardContent>
+              </Card>
+            </Link>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Link to="/schedule">
+              <Card className={classes.card} variant="outlined">
+                <CardContent>
+                  <Grid container spacing={2}>
+                    <Grid item>
+                      <EventIcon style={{ fontSize: 80 }}></EventIcon>
+                    </Grid>
+                    <Grid item xs={12} sm container>
+                      <Grid item xs container direction="column" spacing={2}>
+                        <Grid item xs>
+                          <Typography variant="h6">Agenda</Typography>
+                          <Typography variant="body2">
+                            Simplifique as marcações de consultas com uma agenda
+                            disponível na nuvem e receba estatísticas.
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </CardContent>
+              </Card>
+            </Link>
+          </Grid>
         </Grid>
-      </Grid>
-    </div>
+      </Box>
+    </Container>
   );
 };
